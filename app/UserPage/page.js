561616a2a1ac554f5d146b691788d6f7565d4350
Neto <gildoneto2004@gmail.com>
2025-03-19 17:56:18 -0300

    first commit

--- app/UserPage/page.js
+++ app/UserPage/page.js
@@ -1,7 +1,12 @@
-export default function Layout({ children }) {
-  return (
-    <>
-     <h1> Meu Cu </h1>
-    </>
-  )
+import { NextResponse } from "next/server";
+import path from "path";
+import {WriteFile} from "fs/promises";
+
+export const POST = async (req , res) => {
+  const formData = await req.formData();
+
+  const file = formData.get("file");
+  if (!file){
+    return NextResponse.json({error: "No files recived"}, {status : 400});
+  }
 }
